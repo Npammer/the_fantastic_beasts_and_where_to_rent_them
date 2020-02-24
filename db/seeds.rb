28c4c390dbb6b8750f97b4da8ae0b361a5d304f4
Nikolai Pammer <npammer@gmail.com>
2020-02-25 08:28:38 +1100

    Small ajustments on the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ user1 = User.create!(email: 'harry@mail.com', password: '123456')
 user2 = User.create!(email: 'dumbledore@mail.com', password: '123456')
 user3 = User.create!(email: 'voldemort@mail.com', password: '123456')
 
-puts 'done with users'
+puts 'Created users'
 
 b1 = Beast.create!(name:"Red Dragon", category:"dragon", dangerousness: 4, price: 20, location: "Melbourne", user_id: user1.id)
 b2 = Beast.create!(name:"Giant Spider", category:"insect", dangerousness: 2, price: 17, location: "Sydney", user_id: user2.id)
@@ -19,10 +19,12 @@ b4 = Beast.create!(name:"Pikachu", category:"lightning", dangerousness: 5, price
 b5 = Beast.create!(name:"Chimaera", category:"fire", dangerousness: 5, price: 132, location: "Darwin", user_id: user2.id)
 arr = [b1.id, b2.id, b3.id, b4.id, b5.id]
 
-puts 'done with beasts'
+puts 'Created beasts'
 
 ['fly in 1 straight line', 'spit fire and light up bbq', 'store water in its belly', 'eat venom instead of burger', 'teach children how to swim'].each_with_index do |a, i|
   Ability.create!(description: a, beast_id: arr[i])
 end
 
-puts "ability: #{Ability.all.size}, beast: #{Beast.all.size}, user: #{User.all.size}"
+puts 'Created abilitys'
+
+puts "Successfully created #{Ability.all.size} abilities, #{Beast.all.size} beasts and #{User.all.size} users!"
