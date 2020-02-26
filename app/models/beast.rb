e0ec7200ae83a2faa8d3d5e9fbe129b0bd60e498
minh bui <tranduyminh.bui@gmail.com>
2020-02-26 21:14:17 +1100

    some changes

--- app/models/beast.rb
+++ app/models/beast.rb
@@ -1,11 +1,12 @@
 class Beast < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
-
-  validates :name, :category, :price, presence: true
-  validates :dangerousness, presence: true, inclusion: {in: [1,2,3,4,5]}
+  has_many :bookings, dependent: :destroy
+  has_many :abilities, dependent: :destroy
   has_many :reviews, through: :bookings
-  has_many :abilities
 
   has_one_attached :photo
+  validates :name, :category, :price, presence: true
+  validates :dangerousness, presence: true, inclusion: {in: [1,2,3,4,5]}
+
 end
