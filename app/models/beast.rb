5764c759c481d5bb611dc234c9e5d9d15409a7e0
Nikolai Pammer <npammer@gmail.com>
2020-02-26 14:57:33 +1100

    Map working but not location pins

--- app/models/beast.rb
+++ app/models/beast.rb
@@ -7,5 +7,5 @@ class Beast < ApplicationRecord
   has_many :reviews, through: :bookings
 
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_location?
 end
