05b816bc90c8c88fd6c826c268797db1de1e027a
Nikolai Pammer <npammer@gmail.com>
2020-02-26 14:47:01 +1100

    map woring location pin not working

--- app/models/beast.rb
+++ app/models/beast.rb
@@ -6,5 +6,6 @@ class Beast < ApplicationRecord
   validates :dangerousness, presence: true, inclusion: {in: [1,2,3,4,5]}
   has_many :reviews, through: :bookings
 
-  has_one_attached :photo
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
 end
