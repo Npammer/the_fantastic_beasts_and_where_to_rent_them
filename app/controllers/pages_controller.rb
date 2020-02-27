4e397a8e3414e0641e52f3b32a198aeb0d6c81cd
minh bui <tranduyminh.bui@gmail.com>
2020-02-27 19:43:36 +1100

    added map display to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @beasts = Beast.geocoded.all
+    @markers = @beasts.map do |marker|
+      {
+        lat: marker.latitude,
+        lng: marker.longitude
+      }
+    end
   end
 end
