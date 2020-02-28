3cbc7075eb7ff0ef062d62db607f451407d95e14
minh bui <tranduyminh.bui@gmail.com>
2020-02-28 16:21:27 +1100

    fixed bugs

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,11 +3,6 @@ import mapboxgl from 'mapbox-gl';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  // const fitMapToMarkers = (map, markers) => {
-  //   const bounds = new mapboxgl.LngLatBounds();
-  //   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  //   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  // };
   const fitMapToMarker = (map, marker) => {
     const bounds = new mapboxgl.LngLatBounds();
     bounds.extend([ marker.lng, marker.lat ]);
@@ -21,19 +16,11 @@ const initMapbox = () => {
       style: 'mapbox://styles/minhbui/ck7460q0e2wqo1hk4zeujsi6f'
     });
 
-    // const markers = JSON.parse(mapElement.dataset.markers);
     const marker = JSON.parse(mapElement.dataset.marker);
 
-    // markers.forEach((marker) => {
-    //   new mapboxgl.Marker()
-    //     .setLngLat([ marker.lng, marker.lat ])
-    //     .addTo(map);
-    // });
     new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
-
-
 
     fitMapToMarker(map, marker);
   };
