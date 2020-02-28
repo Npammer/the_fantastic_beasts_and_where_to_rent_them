211e16917a08a39854900b461034a917fa95cacf
minh bui <tranduyminh.bui@gmail.com>
2020-02-28 12:12:29 +1100

    map on homepage and css adjusted

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -45,7 +45,7 @@ const initMapbox2 = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 2000 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 10, duration: 5000 });
   };
 
   if (mapElement2) {
